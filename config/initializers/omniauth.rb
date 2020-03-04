849d71ffcb07e0ab5b993f7aab52c82cd8ad6e58
Jc Vial <jean-charles_vial@hotmail.fr>
2020-03-04 18:18:55 +0100

    add gmail

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
-    {
-      scope: 'userinfo.email, userinfo.profile',
-      prompt: 'select_account',
-      image_aspect_ratio: 'square',
-      image_size: 50
-    }
-end
