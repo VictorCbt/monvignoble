f3462df594ed479e8487d620609914d1c50a4094
geoffroygg <geoffroy2606@gmail.com>
2020-03-04 16:25:00 +0100

    2models

---
+++ db/migrate/20200304135442_create_groups.rb
@@ -0,0 +1,22 @@
+class CreateGroups < ActiveRecord::Migration[6.0]
+  def change
+    create_table :groups do |t|
+      t.string :status
+      t.string :region
+      t.string :designation
+      t.references :service_offered, null: false, foreign_key: true
+      t.string :winemaker_profil
+      t.string :deferred_remunation
+      t.string :services, array: true
+      t.integer :votes
+      t.integer :profitability
+      t.integer :share_available
+      t.integer :investment_total
+      t.integer :ticket_amount
+      t.references :investment, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
