06d67cca6ab1ecc5320fba3dd62fff7075f231f6
VictorCt <victor.c.boncoin@gmail.com>
2020-03-05 12:57:03 +0100

    add-investment controller

---
+++ app/controllers/investments_controller.rb
@@ -0,0 +1,43 @@
+class InvestmentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :show]
+
+  def index
+    @investments = Investment.all
+  end
+
+  def new
+    @investment = Investment.new
+  end
+
+  def create
+    @investment = Investment.new(investments_strong_params)
+    @investment.save
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @investment = Investment.find(params[:id])
+    @investment.destroy!
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+
+
+private
+
+  def investments_strong_params
+    params.require(:investment).permit(:investment_name, :punchline, :winemaker_name, :domaine_name, :description, :bio, :region, :designation, :ticket_amount, :investment_total, :share_available, :total_share, :profitability, :remuneration, :winemaker_profile, :deferred_remuneration, :date_of_end)
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+
+  resources :investments, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
