31dc91ce514c89c4108ebd2387c064df9ec48488
geoffroygg <geoffroy2606@gmail.com>
2020-03-05 17:13:10 +0100

    seed

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :admin, foreign_key: :user_id, class_name: "User"
-  has_many :users_group
-  has_many :user, through: :users_group
+  has_many :users_groups
+  has_many :users, through: :users_groups
   belongs_to :selected_investment, foreign_key: :investment_id , class_name: "Investment"
   has_many :investments_groups, dependent: :destroy
   has_many :investments, through: :investments_groups
