df14501070f2f9f2d25f5fe8f2f8402098bf4732
Jc Vial <jean-charles_vial@hotmail.fr>
2020-03-10 15:14:39 +0100

    update form update group

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,10 +12,26 @@ class Group < ApplicationRecord
     "Visite du domaine",
     "Initiation au processus de vinification"]
 
+  before_validation :clean_empty_services
+
   validates :name, presence: true , uniqueness: true
   validates :region, inclusion: { in: ["Alsace", "Beaujolais", "Bordeaux", "Bourgogne", "Champagne", "Corse", "Jura", "Languedoc-Roussillon", "Provence", "Savoie", "Sud-Ouest", "Vallée de la Loire", "Vallée du Rhône"] }, allow_nil: true
-  validates :services, inclusion: { in: SERVICES }, allow_nil: true
   validates :winemaker_profile, inclusion: { in: ["Jeune vigneron", "Vigneron installé"] }, allow_nil: true
+  validate :services_inclusion
+
+  private
+
+  def clean_empty_services
+    services.reject! { |x| x.blank? }
+  end
+
+  def services_inclusion
+    return if services.blank?
+
+    services.each do |service|
+      errors.add(:services, "'#{service}' not include in authorized services") unless service.in?(SERVICES)
+    end
+  end
 end
 
 
