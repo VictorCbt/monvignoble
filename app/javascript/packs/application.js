2b7b8af5f5f6d838998625ae85b7a80a2781fd09
geoffroygg <geoffroy2606@gmail.com>
2020-03-10 11:23:48 +0100

    add dynamic search on investisments
    - use remote true on form
    - replace only cards on index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,16 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+
+
+
 import "bootstrap";
-
 import { loadDynamicBannerText } from '../components/banner';
-loadDynamicBannerText();
-
+import { initSearch } from '../components/search';
 import '../plugins/init_sweetalert';
-
-
-
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+loadDynamicBannerText();
 AOS.init();
+initSearch();
