05d646b78d342ce57589216f018e57e74678ebb6
Jc Vial <jean-charles_vial@hotmail.fr>
2020-03-11 09:13:08 +0100

    JS epingle en cours

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,9 @@ import { initSearch } from '../components/search';
 import '../plugins/init_sweetalert';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { addInvestmentInFavori } from '../components/investment_selected';
 
 loadDynamicBannerText();
 AOS.init();
 initSearch();
+addInvestmentInFavori();
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, only: [:show, :new, :create, :edit, :update, :destroy] do
+    get "set_favori", on: :member
     resources :users_groups, only: [] do
       collection do
         post :join_request
