a5b18a0a2eed7ee1c42c91b2f7991b0e9ae2fe68
VictorCt <victor.c.boncoin@gmail.com>
2020-03-12 17:35:18 +0100

    avancemernt

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -4,7 +4,7 @@ import 'select2/dist/css/select2.min.css'
 
 
 const initSelect2 = () => {
-  $('.select2').select2({width: '80%'});
+  $('.select2').select2({width: '100%'});
 }
 
 export default initSelect2
