731b8c49a0db70e706a790755fd6e8cc9aa6faaf
VictorCt <victor.c.boncoin@gmail.com>
2020-12-06 08:50:06 +0100

    jquery

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -1,10 +1,13 @@
 import 'select2'
 import 'select2/dist/css/select2.min.css'
 
-
-
 const initSelect2 = () => {
   $('.select2').select2({width: '100%'});
 }
+
+const testSelect2 = () => {
+$(document).ready(function() {
+    $('.testSelect2').select2();
+});
 
 export default initSelect2
